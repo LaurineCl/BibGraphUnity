72493a40fbe65bd21696c167ad0d9db18e2583ae
LaurineCl <lolote.cl@hotmail.fr>
2017-11-30 17:22:47 +0100

    ajout scripts pour les objets ressources

---
+++ Assets/Scripts/GamePlay/ObjetsRessource.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjetsRessource : ObjetsARamasser {
+
+    public string Nom;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
---
+++ Assets/Scripts/GamePlay/_MGR_ObjetsRessource.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class _MGR_ObjetsRessource : MonoBehaviour
+{
+    private static _MGR_ObjetsRessource p_instance = null;              //Static instance of ObjectManager which allows it to be accessed by any other script.
+    public static _MGR_ObjetsRessource Instance { get { return p_instance; } }     // READ ONLY
+
+    Dictionary<string, ObjetsARamasser> p_Dict_ObjetsRamasses;
+    private ObjetsRessource ObjetRessourceCourant;
+
+ 
+    public void AddObject(ObjetsRessource obj)
+    {
+        ObjetRessourceCourant = obj;
+        p_Dict_ObjetsRamasses.Add(obj.Nom, obj);
+    }
+
+    public void RemoveObject(ObjetsRessource obj)
+    {
+        p_Dict_ObjetsRamasses.Remove(obj.Nom);
+    }
+}
